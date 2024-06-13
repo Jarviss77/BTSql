f45a93ed24a3026e1f6dd521b005c2992f086a94
Jarviss77 <gupta.shubham2404@gmail.com>
2024-06-14 03:06:15 +0530

    .dbinfo

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,38 +2,40 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+using namespace std;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here" << std::endl;
+int main(int argc, char* argv[]) {
+    // Flush after every cout / cerr
+    cout << unitbuf;
+    cerr << unitbuf;
+
+    
+    cout << "Logs from your program will appear here" << endl;
 
     if (argc != 3) {
-        std::cerr << "Expected two arguments" << std::endl;
+        cerr << "Expected two arguments" << endl;
         return 1;
     }
 
-    std::string database_file_path = argv[1];
-    std::string command = argv[2];
+    string database_file_path = argv[1];
+    string command = argv[2];
 
     if (command == ".dbinfo") {
-        std::ifstream database_file(database_file_path, std::ios::binary);
+        ifstream database_file(database_file_path, ios::binary);
         if (!database_file) {
-            std::cerr << "Failed to open the database file" << std::endl;
+            cerr << "Failed to open the database file" << endl;
             return 1;
         }
 
-        // Uncomment this to pass the first stage
-        // database_file.seekg(16);  // Skip the first 16 bytes of the header
-        // 
-        // char buffer[2];
-        // database_file.read(buffer, 2);
-        // 
-        // unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
-        // 
-        // std::cout << "database page size: " << page_size << std::endl;
+
+        database_file.seekg(16);  // Skip the first 16 bytes of the header
+        
+        char buffer[2];
+        database_file.read(buffer, 2);
+        
+        unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+        
+        cout << "database page size: " << page_size << endl;
     }
 
     return 0;
